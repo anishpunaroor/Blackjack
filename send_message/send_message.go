ab086b49caf846cb6e70f6e5ce6967b6ceed85c4
anishpunaroor <anish.punaroor@gmail.com>
2021-09-13 12:37:06 -0700

    Set up bot

--- send_message/send_message.go
+++ send_message/send_message.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	api := slack.New(os.Getenv("xoxb-2497620755441-2470350747111-Xr9vBE5SOPtkKIxHB3oensn8"))
+
+	channelID, timestamp, err := api.PostMessage(
+		"C02E5V0H7NZ",
+		slack.MsgOptionText("hello world!", false),
+	)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	fmt.Printf("Message sent successfully to channel %s at %s", channelID, timestamp)
+}
