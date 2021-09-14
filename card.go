4aa5d4d59a979c259191364c92c3b43a9a083d9e
anishpunaroor <anish.punaroor@gmail.com>
2021-09-13 17:39:59 -0700

    Init card deck

--- card.go
+++ card.go
@@ -12,6 +12,8 @@ const (
 	Joker
 )
 
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
+
 type Rank uint8
 
 const (
@@ -31,14 +33,34 @@ const (
 	King
 )
 
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+// Cards belong to a suit and a rank
 type Card struct {
 	Suit
 	Rank
 }
 
+// String representation of a card
 func (c Card) String() string {
 	if c.Suit == Joker {
 		return c.Suit.String()
 	}
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+type Deck []Card
+
+func New() []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+
+	return cards
 }
