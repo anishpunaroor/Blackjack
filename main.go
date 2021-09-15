87c3316658bc4dccad4d148531ee5ea0228589e9
anishpunaroor <anish.punaroor@gmail.com>
2021-09-14 23:08:30 -0700

    Functions with GameState

--- main.go
+++ main.go
@@ -24,6 +24,105 @@ type GameState struct {
 	Dealer Hand
 }
 
+func main() {
+	var gs GameState
+	gs = Shuffle(gs)
+	gs = Deal(gs)
+
+	var input string
+	for gs.State == StatePlayerTurn {
+		fmt.Println("\nPlayer: ", gs.Player)
+		fmt.Println("Dealer: ", gs.Dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			gs = Hit(gs)
+		case "s":
+			gs = Stand(gs)
+		default:
+			fmt.Println("Enter a valid option.")
+		}
+	}
+
+	// If dealer's score is less than 16, we hit. If dealer has a soft 17, then we hit.
+	for gs.State == StateDealerTurn {
+		if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+			gs = Hit(gs)
+		} else {
+			gs = Stand(gs)
+		}
+	}
+	gs = EndHand(gs)
+}
+
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
+	fmt.Println("--FINAL HANDS--")
+	fmt.Println("Player: ", ret.Player, "\nScore: ", pScore)
+	fmt.Println("Dealer: ", ret.Dealer, "\nScore: ", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted")
+	case dScore > 21:
+		fmt.Println("Dealer busted")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case dScore > pScore:
+		fmt.Println("You lose.")
+	case pScore == dScore:
+		fmt.Print("It's a draw.")
+	}
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.NumDeck(3), deck.Shuffle)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
+	var card deck.Card
+	for i := 0; i < 2; i++ {
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
+	}
+	ret.State = StatePlayerTurn
+	return ret
+}
+
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
+
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		return Stand(ret)
+	}
+	return ret
+}
+
+// Draw a card from the top
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
+}
+
 // Print out the suit and rank of all cards in the hand
 func (h Hand) String() string {
 	strs := make([]string, len(h))
@@ -33,6 +132,7 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+// Display the dealer first card in the hand
 func (h Hand) DealerString() string {
 	return h[0].String() + ", HIDDEN"
 }
@@ -65,64 +165,6 @@ func min(a, b int) int {
 	} else {
 		return b
 	}
-}
-
-func main() {
-	cards := deck.New(deck.NumDeck(3), deck.Shuffle)
-	var gs GameState
-	gs.Deck = deck.New(deck.NumDeck(3), deck.Shuffle)
-	var card deck.Card
-	var player, dealer Hand
-	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
-	}
-	var input string
-	for input != "s" {
-		fmt.Println("\nPlayer: ", player)
-		fmt.Println("Dealer: ", dealer.DealerString())
-		fmt.Println("What will you do? (h)it, (s)tand")
-		fmt.Scanf("%s\n", &input)
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		case "s":
-			continue
-		default:
-			fmt.Println("Enter a valid option.")
-		}
-
-	}
-
-	// If dealer's score is less than 16, we hit. If dealer has a soft 17, then we hit.
-	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
-	pScore, dScore := player.Score(), dealer.Score()
-	fmt.Println("--FINAL HANDS--")
-	fmt.Println("Player: ", player, "\nScore: ", pScore)
-	fmt.Println("Dealer: ", dealer, "\nScore: ", dScore)
-	switch {
-	case pScore > 21:
-		fmt.Println("You busted")
-	case dScore > 21:
-		fmt.Println("Dealer busted")
-	case pScore > dScore:
-		fmt.Println("You win!")
-	case dScore > pScore:
-		fmt.Println("You lose.")
-	case pScore == dScore:
-		fmt.Print("It's a draw.")
-	}
-}
-
-// Draw a card from the top
-func draw(cards []deck.Card) (deck.Card, []deck.Card) {
-	return cards[0], cards[1:]
 }
 
 func (gs *GameState) CurrentPlayer() *Hand {
