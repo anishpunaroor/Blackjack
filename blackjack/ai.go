c21049eeacbff717b96f2346169abc09f6c6a4b3
anishpunaroor <anish.punaroor@gmail.com>
2021-09-15 10:53:54 -0700

    Add AI

---
+++ blackjack/ai.go
@@ -0,0 +1,47 @@
+package blackjack
+
+import (
+	"fmt"
+
+	"github.com/anishpunaroor/Blackjack/deck"
+)
+
+type AI interface {
+	Bet() int
+	Play(hand []deck.Card, dealer deck.Card) Move
+	Results(hand [][]deck.Card, dealer []deck.Card)
+}
+
+type dealerAI struct{}
+
+func (ai dealerAI) Bet() int {
+	return 1
+}
+
+func (ai dealerAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+
+}
+
+type humanAI struct{}
+
+func HumanAI() AI {
+	return humanAI{}
+}
+
+func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	for {
+		fmt.Println("\nPlayer: ", hand)
+		fmt.Println("Dealer: ", dealer)
+		fmt.Println("What will you do? (h)it, (s)tand")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return MoveHit
+		case "s":
+			return MoveStand
+		default:
+			fmt.Println("Enter a valid option.")
+		}
+	}
+}
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -62,7 +62,7 @@ func TestFilter(t *testing.T) {
 }
 
 func TestDeck(t *testing.T) {
-	cards := New(Deck(2))
+	cards := New(NumDeck(2))
 	if len(cards) != 13*4*2 {
 		t.Errorf("Expected %d cards, received %d cards.", 13*4*2, len(cards))
 	}
